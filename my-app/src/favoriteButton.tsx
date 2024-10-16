9dc2da6b1044ace7397869408cd899bc487cca43
Yashwanth Ravipati <yashravipatischool@gmail.com>
2024-10-16 02:43:39 -0700

    Fixing favoriteButton for test

--- my-app/src/favoriteButton.tsx
+++ my-app/src/favoriteButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Note } from './types';
 import { useContext } from 'react';
 import { NotesContext } from './NotesContext';
@@ -7,23 +7,18 @@ function FavoriteButton({ note }: { note: Note }) {
     const [isFavorite, setIsFavorite] = useState(note.favorite);
     const { notes, setNotes } = useContext(NotesContext);
 
-    const toggleFavorite = () => {
-        setIsFavorite(!isFavorite);
-        if (isFavorite) {
-            const index = notes.indexOf(note.title);
-            if (index > -1) {
-                notes.splice(index, 1);
-            }
-        } else {
-            notes.push(note.title);
-        }
-        console.log(notes);
-    };
+    const toggleFavorite = useCallback(() => {
+        setIsFavorite((prevFavorite) => !prevFavorite);
+    }, []);
 
     useEffect(() => {
-        note.favorite = isFavorite; // Update the 'favorite' property of the note instance
-        setNotes([...notes]);
-    }, [isFavorite, note, setNotes, notes]);
+        if (isFavorite && !notes.includes(note.title)) {
+            setNotes([...notes, note.title]);
+        }
+        else if (!isFavorite && notes.includes(note.title)) {
+            setNotes(notes.filter((title) => title !== note.title));
+        }
+    }, [isFavorite, note.title, notes, setNotes]);
 
     return (
         <button onClick={toggleFavorite}>{isFavorite ? '❤️' : '♡'}</button>
